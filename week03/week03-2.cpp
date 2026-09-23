3172c9866f5d847955101f87d1351938d8c4257f
14750216 <angel.ch20070720@gmail.com>
2026-09-23 11:36:20 +0800

    week03

---
+++ week03/week03-2.cpp
@@ -0,0 +1,20 @@
+// week03-2.cpp 學習計畫 Basic 第6題
+//LeetCode 283. Move Zeroes
+//比0移到右邊去,等於(不適0的放左邊)再補0
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int k = 0; // 目標放在哪裡 nums[k]
+        for (int num : nums){ //C++進階for迴圈
+            if (num !=0){
+                nums[k] = num; //把數字放左邊
+                k++; //換下一格
+            }
+        }
+        //把殘留的,都變成0
+        for (int i=k; i<nums.size(); i++){
+            nums[i] = 0;
+        }
+
+    }
+};
